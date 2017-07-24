0735ec1279db26a9e39988d54eed27b5264fafaa
Kevin Beronilla <kevin.beronilla@gmail.com>
2017-07-24 11:14:41 -0500

    Added explicit paths in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ var gulp = require('gulp'),
     rename = require('gulp-rename');
 
 gulp.task('minify-css', function() {
-    return gulp.src(['css/**/*.css', '!css/**/*.min.css'])
+    return gulp.src(['./css/**/*.css', '!./css/**/*.min.css'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifyCss({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('./css'))
 });
 
 gulp.task('uglify', function() {
-    return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+    return gulp.src(['./js/**/*.js', '!./js/**/*.min.js'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('./js'))
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['css/**/*.css', '!css/**/*.min.css'], ['minify-css']);
-    gulp.watch(['js/**/*.js', '!js/**/*.min.js'], ['uglify']);
+    gulp.watch(['./css/**/*.css', '!./css/**/*.min.css'], ['minify-css']);
+    gulp.watch(['./js/**/*.js', '!./js/**/*.min.js'], ['uglify']);
 });
 
 gulp.task('default', ['minify-css', 'uglify', 'watch']);
