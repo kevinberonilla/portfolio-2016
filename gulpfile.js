d6df1b63f64bdc23f85d1dcd8e30fb8ebe796025
Kevin Beronilla <kevin.beronilla@gmail.com>
2017-11-20 10:02:23 -0600

    Updated gulp task names

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,23 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
-gulp.task('minify-css', function() {
+gulp.task('minify:css', function() {
     return gulp.src(['./css/**/*.css', '!./css/**/*.min.css'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifyCss({ compatibility: 'ie8' }))
         .pipe(gulp.dest('./css'))
 });
 
-gulp.task('uglify', function() {
+gulp.task('minify:js', function() {
     return gulp.src(['./js/**/*.js', '!./js/**/*.min.js'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .pipe(gulp.dest('./js'))
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['./css/**/*.css', '!./css/**/*.min.css'], ['minify-css']);
-    gulp.watch(['./js/**/*.js', '!./js/**/*.min.js'], ['uglify']);
+gulp.task('init:watch', function() {
+    gulp.watch(['./css/**/*.css', '!./css/**/*.min.css'], ['minify:css']);
+    gulp.watch(['./js/**/*.js', '!./js/**/*.min.js'], ['minify:js']);
 });
 
-gulp.task('default', ['minify-css', 'uglify', 'watch']);
+gulp.task('default', ['minify:css', 'minify:js', 'init:watch']);
